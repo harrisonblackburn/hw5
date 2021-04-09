bdace071cf89d901abdbe2c839a402a6cb02edf7
harrisonblackburn <blackbhr@yahoo.com>
2021-04-09 15:25:44 -0500

    all save buttons working

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ $('#currentDay')
 
 
 
+
 function updateClock() {
     var day = moment().format("MMM Do YY");
     $("#currentTime").text(day);
@@ -18,7 +19,70 @@ $("#9btn").on("click", function(){
     alert(nineAM);
 });
 
+var tenAM = $('#10amInput').val();
 
+$("#10btn").on("click", function(){
+    var nineAM = $("#10amInput").val();
+    alert(tenAM);
+});
+
+var elevenAM = $('#11amInput').val();
+
+$("#11btn").on("click", function(){
+    var nineAM = $("#11amInput").val();
+    alert(elevenAM);
+});
+
+var twelvePM = $('#12pmInput').val();
+
+$("#12btn").on("click", function(){
+    var twelvePM = $("#12pmInput").val();
+    alert(twelvePM);
+});
+
+var onePM = $('#1pmInput').val();
+
+$("#1btn").on("click", function(){
+    var onePM = $("#1pmInput").val();
+    alert(onePM);
+});
+
+var twoPM = $('#2pmInput').val();
+
+$("#2btn").on("click", function(){
+    var twoPM = $("#2pmInput").val();
+    alert(twoPM);
+});
+
+var threePM = $('#3pmInput').val();
+
+$("#3btn").on("click", function(){
+    var nineAM = $("#3pmInput").val();
+    alert(threePM);
+});
+
+var fourPM = $('#4pmInput').val();
+
+$("#4btn").on("click", function(){
+    var fourPM = $("#4pmInput").val();
+    alert(fourPM);
+});
+
+var fivePM = $('#5pmInput').val();
+
+$("#5btn").on("click", function(){
+    var fivePM = $("#5pmInput").val();
+    alert(fivePM);
+});
 // Need function to store saved "to-dos" in local storage:
 // localStorage.getItem("count"); 
 // local storage.setItem ??? 
+
+// need a for loop to change time-slot BG color as day progresses 
+
+
+// need event listener for save button 
+
+
+
+// then, will store in local storage
